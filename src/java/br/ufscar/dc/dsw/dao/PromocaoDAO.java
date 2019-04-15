2170dfa8f15fb7fb9dfd09506f416d6e695bd83b
DaanMoura <daniel.lucio.moura@gmail.com>
2019-04-15 11:36:53 -0300

    corrigido endereço do BD. PromocaoDAO iniciado

--- src/java/br/ufscar/dc/dsw/dao/PromocaoDAO.java
+++ src/java/br/ufscar/dc/dsw/dao/PromocaoDAO.java
@@ -5,10 +5,24 @@
  */
 package br.ufscar.dc.dsw.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author daniel
  */
 public class PromocaoDAO {
+    public PromocaoDAO() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
     
+    protected Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:derby://localhost:1527/VendaIngressoBD", "root", "root");
+    }
 }
